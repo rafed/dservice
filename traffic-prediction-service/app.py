a638a321fca5d0135cd4dadf3b6721f9a512900e
Moumita Asad <bsse0731@iit.du.ac.bd>
2019-05-08 00:04:30 +0600

    traffic started

--- traffic-prediction-service/app.py
+++ traffic-prediction-service/app.py
@@ -1,30 +1,36 @@
 from flask import Flask, request
+import requests
 import datetime
 
 app = Flask(__name__)
 
-@app.route('/predictTrafficJam', methods=['GET'])
+@app.route('/predictTrafficJam', methods=['POST'])
 def predictTrafficJam():
     today = datetime.datetime.today() 
 
     try:
         data = {
-            'src': request.args['src'],
-            'dst' : request.args['dst'],
-            'dateFrom' : today,
-            'dateTo' : (today - datetime.timedelta(6))
+            'src': "Adarsh Nagar Main Road",#request.form['src'],
+            'dst' : "Ashoka Road/Shah Alam Bandh Marg/Jahangirpuri Main Road" ,#request.form['dst'],
+            'dateTo' : today.strftime('%d-%m-%Y'),
+            'dateFrom' : (today - datetime.timedelta(1)).strftime('%d-%m-%Y') #(today - datetime.timedelta(6))
         }
         
-        url = "http://road-information-service:8000/getRoadData"
+        url = "http://172.16.21.70:9999/getRoadData"
         response = requests.post(url,data=data)
+        print(response.text)
+        # columnNames = ['date', 'time', 'weekday', 'li', 'pc', 'src', 'dst', 'su', 'ff', 'temperature', 'daylight', 'humidity', 'raindesc', 'rainfall', 'windspeed', 'holiday', 'area', 'jf']
+        # pd.DataFrame(response, columns=columnNames)
 
         return response
     except Exception as e:
-        print("Database error")
+        print(e)
 
-@app.route('/')
-def hello():
-    return "Hello world"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+predictTrafficJam()
+# @app.route('/')
+# def hello():
+#     return "Hello world"
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=8000)
